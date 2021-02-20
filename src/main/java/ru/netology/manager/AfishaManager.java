5ab43c958f1fb27d11b7be600e7627e877c9bb9a
kseniabobkova <bobkova@healbe.com>
2021-02-20 22:49:31 +0300

    movie

---
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -0,0 +1,57 @@
+package ru.netology.manager;
+
+import ru.netology.domain.MovieItem;
+
+public class AfishaManager {
+    private MovieItem[] items = new MovieItem[0];
+
+    public void add(MovieItem item) {
+        // создаём новый массив размером на единицу больше
+        int length = items.length + 1;
+        MovieItem[] tmp = new MovieItem[length];
+        // itar + tab
+        // копируем поэлементно
+        // for (int i = 0; i < items.length; i++) {
+        //   tmp[i] = items[i];
+        // }
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        // кладём последним наш элемент
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
+    }
+
+    public MovieItem[] getAll() {
+        MovieItem[] result = new MovieItem[items.length];
+        // перебираем массив в прямом порядке
+        // но кладём в результаты в обратном
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        if (result.length < 10){
+        return result;}
+        else {
+            MovieItem [] result1 = new MovieItem[10];
+            for (int i = 0; i <= 9; i++) {
+                result1[i] = result[i];}
+                return result1;
+            }
+        }
+
+
+    // наивная реализация
+    public void removeById(int id) {
+        int length = items.length - 1;
+        MovieItem[] tmp = new MovieItem[length];
+        int index = 0;
+        for (MovieItem item : items) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        }
+        // меняем наши элементы
+        items = tmp;
+    }
+}gi
